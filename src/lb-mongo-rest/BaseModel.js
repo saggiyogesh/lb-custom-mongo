f9b1b5330035aac432c1118112af2c6eefce972f
Dindrawat <disha.indrawat@gmail.com>
2018-05-04 12:11:59 +0530

    Update BaseModel.js

--- src/lb-mongo-rest/BaseModel.js
+++ src/lb-mongo-rest/BaseModel.js
@@ -156,10 +156,11 @@ class BaseModel extends mix(LeanMongooseFinders, NativeMongo) {
     // id = coerceId(id);
     const currentInstance = this._doc;
     const where = { id };
-    const _data = Object.assign({}, data, { id });
-    await this.constructor.beforeUpdate({ currentInstance, where, data: _data });
-    const instance = await this.constructor.findOneAndUpdate({ _id: id }, { data: _data }, { new: true });
-    await this.constructor.afterUpdate({ instance, where, data: _data });
+    // const _data = Object.assign({}, data, { id });
+    data.id = id;
+    await this.constructor.beforeUpdate({ currentInstance, where, data });
+    const instance = await this.constructor.findOneAndUpdate({ _id: id }, data, { new: true });
+    await this.constructor.afterUpdate({ instance, where, data });
     return instance;
   }
 }
